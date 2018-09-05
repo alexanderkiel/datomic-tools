eb7d57f5076f23e61a7e6b9fb690db5b1c0298b0
Alexander Kiel <alexanderkiel@gmx.net>
2018-09-05 18:06:53 +0200

    Update Dependencies

--- project.clj
+++ project.clj
@@ -7,8 +7,8 @@
   :min-lein-version "2.0.0"
   :pedantic? :abort
 
-  :dependencies [[org.clojars.akiel/datomic-spec "0.1-alpha19"]
+  :dependencies [[org.clojars.akiel/datomic-spec "0.2"]
                  [org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.specs.alpha "0.2.36"]]
+                 [org.clojure/core.specs.alpha "0.2.44"]]
 
-  :profiles {:dev {:dependencies [[com.datomic/datomic-free "0.9.5656"]]}})
+  :profiles {:dev {:dependencies [[com.datomic/datomic-free "0.9.5697"]]}})
